8c59a650aefc4d147a5f2d57ea743f00cb01eb47
CasperHogstrom <casperhogstrom3@gmail.com>
2022-10-04 09:46:28 +0200

    The server is a static file server

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
+const fs = require('fs');
 const http = require('http');
+const path = require('path');
 
-const hostname = '127.0.0.1';
-const port = 8080;
+const port = 8080
+const directoryName = './public';
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello from Node</h1>');
+const types = {
+    html: 'text/html',
+    js: 'application/javascript'
+};
+
+
+const root = path.normalize(path.resolve(directoryName));
+
+const server = http.createServer((req, res) => {
+  console.log(`${req.method} ${req.url}`);
+
+  const extension = path.extname(req.url).slice(1);
+  const type = extension ? types[extension] : types.html;
+  const supportedExtension = Boolean(type);
+
+  if (!supportedExtension) {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('404: File not found');
+    return;
+  }
+
+  let fileName = req.url;
+  if (req.url === '/') fileName = 'index.html';
+  else if (!extension) {
+    try {
+      fs.accessSync(path.join(root, req.url + 'index.html'), fs.constants.F_OK);
+      fileName = req.url + 'index.html';
+    } catch (e) {
+      fileName = path.join(req.url, '.html');
+    }
+  }
+
+  const filePath = path.join(root, fileName);
+  const isPathUnderRoot = path
+    .normalize(path.resolve(filePath))
+    .startsWith(root);
+
+  if (!isPathUnderRoot) {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('404: File not found');
+    return;
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('404: File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': type });
+      res.end(data);
+    }
+  });
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log('Server is running');
 });
